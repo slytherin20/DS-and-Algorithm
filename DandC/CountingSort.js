0c2998bda16e0ec95dd3952f788fe153d3c7f2e7
slytherin20 <sonaligupta2904@gmail.com>
2020-07-30 13:48:33 +0530

    Stable version

--- DandC/CountingSort.js
+++ DandC/CountingSort.js
@@ -21,6 +21,25 @@ return output;
 }
 
 //Stable countSort
+function stableCount(a,k){
+    let count=[];
+    let output=[];
+    for(let i=0;i<=k;i++){
+        count[i] = 0;
+    }
+
+    for(let i=0;i<a.length;i++){
+        count[a[i]] = count[a[i]]+1;
+    }
+    for(let i=1;i<=k;i++){
+        count[i] = count[i-1]+count[i];
+    }
+    for(let i = a.length-1;i>=0;i--){
+        output[--count[a[i]]] = a[i];
+    }
+
+return count;
+}
 
 
-console.log(countingSort([1,4,1,2,0,7,5,2],9));
+console.log(stableCount([1,4,1,2,0,7,5,2],9));
