a5e85a23b2fd93a86c6f1870f04d1d47ece86547
slytherin20 <sonaligupta2904@gmail.com>
2020-07-30 16:06:16 +0530

    Negative counting sort

--- DandC/CountingSort.js
+++ DandC/CountingSort.js
@@ -34,7 +34,7 @@ function stableCount(a,k){
     for(let i=1;i<=k;i++){
         count[i] = count[i-1]+count[i];
     }
-    for(let i = a.length-1;i>=0;i--){
+    for(let i = a.length-1;i>=0;i--){                   //Traversing from last element in array 'a'
         output[--count[a[i]]] = a[i];
     }
 
@@ -42,4 +42,36 @@ return count;
 }
 
 
-console.log(stableCount([1,4,1,2,0,7,5,2],9));
+//To sort including negative numbers as well.
+function negativeCount(a){
+    let count=[];
+    let output=[];
+    let minimum = Math.min(...a);
+    let maximum = Math.max(...a);
+    let range = maximum-minimum+1;
+
+
+    for(let i=0;i<range;i++){
+        count[i] = 0;
+    }
+
+    for(let i = 0;i<a.length;i++){
+        count[a[i]-minimum]++;
+    }
+    for(let i=1;i<range;i++){
+        count[i]+= count[i-1];
+    }
+    for(let i=a.length-1;i>=0;i--){
+        output[--count[a[i]-minimum]] = a[i];
+    }
+
+
+return output;
+
+}
+
+
+console.log(negativeCount([-8,-2,-3,-8,1,4,1,2,0,7,5,2]));
+
+//T.C = O(n+k)
+//Auxilary space = O(n+k)
